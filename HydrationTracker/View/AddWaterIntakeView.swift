ef4d67d01674ba1a19c1a0365c1b64e3603e9dc0
Venkatesh Nyamagoudar <vnyamagoudar@gmail.com>
2024-03-18 03:54:39 +0530

    Implented addview and editview. Need to implement Coredata for persistance and Statistics view

---
+++ HydrationTracker/View/AddWaterIntakeView.swift
@@ -0,0 +1,18 @@
+//
+//  AddWaterIntakeView.swift
+//  HydrationTracker
+//
+//  Created by Venkatesh Nyamagoudar on 3/18/24.
+//
+
+import SwiftUI
+
+struct AddWaterIntakeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddWaterIntakeView()
+}
